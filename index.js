b1498c469cb97f782fb76dce73b85af82f64391e
Rajesh <choudharyrajesh729@gmail.com>
2023-06-04 22:51:40 +0530

    port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path')
 const {createRestApi} = require('./backend/api');
 const {createViewApi} = require('./fronted/api');
 
-const port = 3000;           // port used to run server
+const port = 8080;           // port used to run server
 const app = express();
 
 app.use(express.json());
