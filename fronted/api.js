c3efc4601d4f0b631ef1773717fda73c879e6b56
Rajesh <choudharyrajesh729@gmail.com>
2023-06-05 12:26:06 +0530

    app.use

--- fronted/api.js
+++ fronted/api.js
@@ -3,7 +3,8 @@ const path = require('path');
 const createViewApi = app => {
     app.get('/', async (request, response) => {
         if (request.session.userId === 1) {
-            return response.sendFile(path.join(__dirname, 'superAdminSite.html'));
+            // return response.sendFile(path.join(__dirname, 'superAdminSite.html'));
+            return app.use(express.static('superAdminSite.html'))
         }
         else if (request.session.userId) {
             return response.sendFile(path.join(__dirname, 'site.html'));
